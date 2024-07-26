cf04d66e928992e24fe68b3bb40b693027f8fb93
Type3limit <582268725@qq.com>
2024-07-26 17:15:16 +0800

    添加项目文件。

---
+++ Models/DisplayModel/GlobalDisplaySettingModel.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Imaging;
+
+namespace DesktopTimerWPFUIRemake.models.displayModel
+{
+    public class GlobalDisplaySettingModel:ObservableObject
+    {
+        #region Properties
+
+
+        #endregion
+    }
+}
---
+++ Models/MainWorkModel.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using DesktopTimerWPFUIRemake.models.displayModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopTimerWPFUIRemake.models
+{
+    public class MainWorkModel: ObservableObject
+    {
+        #region Properties
+
+        private GlobalDisplaySettingModel? displaySettingModel = null;
+        /// <summary>
+        /// For Ui display properties setting;
+        /// </summary>
+        public GlobalDisplaySettingModel DisplaySettingModel
+        {
+            get=>displaySettingModel??(displaySettingModel = new GlobalDisplaySettingModel());
+        }
+
+        #endregion
+    }
+}
